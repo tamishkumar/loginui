5f9c2e755a4d14dd79480f517b2e5b3514101a51
Karan <karan.jain@infostride.com>
2019-07-18 22:40:34 +0530

    Updates

---
+++ precache-manifest.1c3d09734f289b6f890873f1a5f010e2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb2806af8e20a00cfcd",
+    "url": "/static/css/5.c3b73d3f.chunk.css"
+  },
+  {
+    "revision": "a3f3d358aee4bad69f2f",
+    "url": "/static/css/0.76a3f541.chunk.css"
+  },
+  {
+    "revision": "e76cce4ec980807a27d9",
+    "url": "/static/css/main.d7975c57.chunk.css"
+  },
+  {
+    "revision": "e76cce4ec980807a27d9",
+    "url": "/static/js/main.e76cce4e.chunk.js"
+  },
+  {
+    "revision": "d0ac72a3ccc8d638f071",
+    "url": "/static/css/2.1e67c6e4.chunk.css"
+  },
+  {
+    "revision": "d0ac72a3ccc8d638f071",
+    "url": "/static/js/2.d0ac72a3.chunk.js"
+  },
+  {
+    "revision": "1ab4081c0c714fdeb33b",
+    "url": "/static/js/3.1ab4081c.chunk.js"
+  },
+  {
+    "revision": "4537c7d1911d1eeb6e3b",
+    "url": "/static/js/4.4537c7d1.chunk.js"
+  },
+  {
+    "revision": "a3f3d358aee4bad69f2f",
+    "url": "/static/js/0.a3f3d358.chunk.js"
+  },
+  {
+    "revision": "4bb2806af8e20a00cfcd",
+    "url": "/static/js/5.4bb2806a.chunk.js"
+  },
+  {
+    "revision": "89c04349cad017a9804b",
+    "url": "/static/css/6.58e5a6fc.chunk.css"
+  },
+  {
+    "revision": "89c04349cad017a9804b",
+    "url": "/static/js/6.89c04349.chunk.js"
+  },
+  {
+    "revision": "d03c6bbda5833ca08558",
+    "url": "/static/js/7.d03c6bbd.chunk.js"
+  },
+  {
+    "revision": "917f12b25d8e09aa340e",
+    "url": "/static/js/8.917f12b2.chunk.js"
+  },
+  {
+    "revision": "d32f3cf240e5afe520ad",
+    "url": "/static/js/runtime~main.d32f3cf2.js"
+  },
+  {
+    "revision": "1a76184307693018c826631d4e6b70e8",
+    "url": "/static/media/blueimg.1a761843.jpg"
+  },
+  {
+    "revision": "a4a0a5acae2c9c5c7dde5e0b6fa1b5e2",
+    "url": "/index.html"
+  }
+];
